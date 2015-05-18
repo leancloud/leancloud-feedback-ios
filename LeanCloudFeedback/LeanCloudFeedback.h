ea70e35ee960140d09662f025c26c8a49e879e74
junwen <jwfing@gmail.com>
2015-05-18 13:37:34 +0800

    empty proj

---
+++ LeanCloudFeedback/LeanCloudFeedback.h
@@ -0,0 +1,19 @@
+//
+//  LeanCloudFeedback.h
+//  LeanCloudFeedback
+//
+//  Created by Feng Junwen on 5/18/15.
+//  Copyright (c) 2015 LeanCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeanCloudFeedback.
+FOUNDATION_EXPORT double LeanCloudFeedbackVersionNumber;
+
+//! Project version string for LeanCloudFeedback.
+FOUNDATION_EXPORT const unsigned char LeanCloudFeedbackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanCloudFeedback/PublicHeader.h>
+
+
