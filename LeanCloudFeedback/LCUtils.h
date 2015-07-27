119dc5921ff38a78fcd5f2f0f542ed714b5ed944
lzwjava <lzwjava@gmail.com>
2015-07-27 11:23:55 +0800

    change to FLog(fmt, ...) to keep consistency

--- LeanCloudFeedback/LCUtils.h
+++ LeanCloudFeedback/LCUtils.h
@@ -12,7 +12,7 @@
 #ifdef DEBUG
 #   define FLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
 #else
-#   define FLog(...)
+#   define FLog(fmt, ...)
 #endif
 
 @interface LCUtils : NSObject
